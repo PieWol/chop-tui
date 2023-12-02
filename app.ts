674f126e7c71e48f8798fff809e49af9f318d595
Piet Wolff <piet.wolff@gmail.com>
2023-12-02 22:40:39 +0100

    decode everything

--- app.ts
+++ app.ts
@@ -21,7 +21,8 @@ async function getRuntimeUpgradeByProposalHash(network: string) {
   const proposalLen = readlineSync.question('Enter the len: ');
 
   try {
-    const encodedCall = (await api.query.preimage.preimageFor([proposalHash, proposalLen])).toHuman();
+    const preimage = await api.query.preimage.preimageFor([proposalHash, proposalLen]);
+    const encodedCall = preimage.toHuman();
 
     // Display the results
     const metadata = await api.rpc.state.getMetadata();
@@ -29,10 +30,35 @@ async function getRuntimeUpgradeByProposalHash(network: string) {
 
     // Decode the extrinsic using the extrinsic type
     const decodedExtr = api.createType("Call", encodedCall).toHuman();
-    console.log('Decoded Extrinsic:', decodedExtr);
+    console.log('Decoded Extrinsic:', formatCall(decodedExtr));
   } catch (error) {
     console.error(`Error fetching data for proposal hash ${proposalHash}:`, error);
   }
+}
+
+function formatCall(call: any, depth = 0): string {
+  let result = '';
+
+  for (const key in call) {
+    if (call.hasOwnProperty(key)) {
+      const value = call[key];
+
+      if (Array.isArray(value)) {
+        // Handle arrays (e.g., nested calls)
+        value.forEach((item: any) => {
+          result += `${' '.repeat(depth * 2)}${key}:\n${formatCall(item, depth + 1)}`;
+        });
+      } else if (typeof value === 'object') {
+        // Handle nested objects
+        result += `${' '.repeat(depth * 2)}${key}:\n${formatCall(value, depth + 1)}`;
+      } else {
+        // Display key-value pair
+        result += `${' '.repeat(depth * 2)}${key}: ${value}\n`;
+      }
+    }
+  }
+
+  return result;
 }
 
 // Prompt the user for the network selection
